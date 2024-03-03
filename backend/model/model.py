719a0b699b73f0bab7a14cce97e53b6d86c6abc4
Kinga Świderek <kinga.swiderek2.stud@pw.edu.pl>
2024-03-03 23:05:59 +0100

    Refactor model.py

--- backend/model/model.py
+++ backend/model/model.py
@@ -6,71 +6,86 @@ IMG_SHAPE = IMG_SIZE + (3,)
 BATCH_SIZE = 32
 AUTOTUNE = tf.data.AUTOTUNE
 
-train_dataset = tf.keras.utils.image_dataset_from_directory("data/train",
-                                                            shuffle=True,
-                                                            batch_size=BATCH_SIZE,
-                                                            image_size=IMG_SIZE,
-                                                            labels="inferred",
-                                                            label_mode="categorical")
-valid_dataset = tf.keras.utils.image_dataset_from_directory("data/valid",
-                                                            shuffle=True,
-                                                            batch_size=BATCH_SIZE,
-                                                            image_size=IMG_SIZE,
-                                                            labels="inferred",
-                                                            label_mode="categorical")
-test_dataset = tf.keras.utils.image_dataset_from_directory("data/test",
-                                                           shuffle=True,
-                                                           batch_size=BATCH_SIZE,
-                                                           image_size=IMG_SIZE,
-                                                           labels="inferred",
-                                                           label_mode="categorical")
 
-train_dataset = train_dataset.prefetch(buffer_size=AUTOTUNE)
-valid_dataset = valid_dataset.prefetch(buffer_size=AUTOTUNE)
-test_dataset = test_dataset.prefetch(buffer_size=AUTOTUNE)
+def get_image_dataset(directory: str):
+    return tf.keras.utils.image_dataset_from_directory(
+        directory,
+        shuffle=True,
+        batch_size=BATCH_SIZE,
+        image_size=IMG_SIZE,
+        labels="inferred",
+        label_mode="categorical"
+    )
 
-data_augmentation = tf.keras.Sequential([
-  tf.keras.layers.RandomFlip('horizontal'),
-  tf.keras.layers.RandomRotation(0.2),
-])
 
-preprocess_input = tf.keras.applications.mobilenet_v2.preprocess_input
+def create_model(learning_rate: float):
+    # adding random flips and rotations to acquire more training samples
+    data_augmentation = tf.keras.Sequential([
+        tf.keras.layers.RandomFlip('horizontal'),
+        tf.keras.layers.RandomRotation(0.2),
+    ])
 
-base_model = tf.keras.applications.MobileNetV2(input_shape=IMG_SHAPE,
-                                               include_top=False,
-                                               weights='imagenet')
-base_model.trainable = False
+    # knowledge transfer from MobileNetV2
+    preprocess_input = tf.keras.applications.mobilenet_v2.preprocess_input
+    base_model = tf.keras.applications.MobileNetV2(input_shape=IMG_SHAPE,
+                                                   include_top=False,
+                                                   weights='imagenet')
+    base_model.trainable = False
 
-global_average_layer = tf.keras.layers.GlobalAveragePooling2D()
+    global_average_layer = tf.keras.layers.GlobalAveragePooling2D()
 
-prediction_layer = tf.keras.layers.Dense(70, activation="softmax")
+    prediction_layer = tf.keras.layers.Dense(70, activation="softmax")
 
-inputs = tf.keras.Input(shape=(160, 160, 3))
-x = data_augmentation(inputs)
-x = preprocess_input(x)
-x = base_model(x, training=False)
-x = global_average_layer(x)
-x = tf.keras.layers.Dropout(0.2)(x)
-outputs = prediction_layer(x)
-model = tf.keras.Model(inputs, outputs)
+    # putting together all layers
+    inputs = tf.keras.Input(shape=IMG_SHAPE)
+    x = data_augmentation(inputs)
+    x = preprocess_input(x)
+    x = base_model(x, training=False)
+    x = global_average_layer(x)
+    x = tf.keras.layers.Dropout(0.25)(x)
+    outputs = prediction_layer(x)
+    model = tf.keras.Model(inputs, outputs)
 
-print(model.summary())
+    model.compile(
+        optimizer=tf.keras.optimizers.legacy.Adam(learning_rate=learning_rate),
+        loss=tf.keras.losses.CategoricalCrossentropy(),
+        metrics=["accuracy"]
+    )
 
-base_learning_rate = 0.0001
-model.compile(optimizer=tf.keras.optimizers.legacy.Adam(learning_rate=base_learning_rate),
-              loss=tf.keras.losses.CategoricalCrossentropy(),
-              metrics=["accuracy"])
+    return model
 
-history = model.fit(train_dataset,
-                    epochs=20,
-                    validation_data=valid_dataset)
 
-model.save("dog_breeds.keras")
+def fit_model(model, epochs, train_dir="data/train", val_dir="data/valid"):
+    # load datasets
+    train_dataset = get_image_dataset(train_dir)
+    valid_dataset = get_image_dataset(val_dir)
 
-plt.plot(history.history['accuracy'], label='accuracy', linewidth=2)
-plt.plot(history.history['val_accuracy'], label='val_accuracy', linewidth=2)
-plt.legend()
-plt.xlabel('Epochs')
-plt.ylabel('Score')
-plt.title('Accuracy')
-plt.savefig("accuracy.png")
+    # prefetch for better performance
+    train_dataset = train_dataset.prefetch(buffer_size=AUTOTUNE)
+    valid_dataset = valid_dataset.prefetch(buffer_size=AUTOTUNE)
+
+    return model.fit(train_dataset,
+                     epochs=epochs,
+                     validation_data=valid_dataset)
+
+
+def test_model(model, test_dir="data/test"):
+    test_dataset = get_image_dataset(test_dir)
+    test_dataset = test_dataset.prefetch(buffer_size=AUTOTUNE)
+    return model.evaluate(test_dataset)
+
+
+if __name__ == "__main__":
+    model = create_model(0.0001)
+    history = fit_model(model, 25)
+    model.save("dog_breeds.keras")
+
+    _, test_acc = test_model(model)
+
+    plt.plot(history.history['accuracy'], label='accuracy', linewidth=2)
+    plt.plot(history.history['val_accuracy'], label='val_accuracy', linewidth=2)
+    plt.legend()
+    plt.xlabel('Epochs')
+    plt.ylabel('Score')
+    plt.title('Accuracy')
+    plt.savefig("accuracy.png")
